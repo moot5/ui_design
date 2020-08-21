a628f9f40d0ac7365d24ffabeb9a00c51d16b5f4
Martin <m.harris13@wlv.ac.uk>
2020-08-21 16:54:36 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash/animated_splash.dart';
+import 'package:ui_design/search_r.dart';
+
+void main() {
+  //runApp(MyApp());
+  runApp(MaterialApp(
+    home: AnimatedSplash(
+      imagePath: 'assets/images/1.jpg',
+      home: MyApp(),
+      duration: 2500,
+      type: AnimatedSplashType.StaticDuration,
+    ),
+  ));
+}
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: TextTheme(
+            bodyText1: TextStyle(),
+            bodyText2: TextStyle(),
+          ).apply(
+            bodyColor: Colors.black,
+          ),
+      ),
+      routes: {
+        '/search': (context) => search_r(),
+      },
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> dataholder = [
+    "Johnette Otterson",
+    "Sara Easler",
+    "Anna Ayala",
+    "Lamonica Uphoff",
+    "Shakira Wolff",
+    "Belle Abasta",
+    "Christena Brownstein",
+    "Grady Straker",
+    "Roxann Ashbaugh",
+    "Kyong Frank",
+    "Angla Colunga",
+    "Matha Flemister",
+    "Felisha Premo",
+    "Bernadette Kelly",
+    "Glennis Bierbaum",
+    "Etha Sessoms",
+    "Kai Selders",
+    "Celsa Yee",
+    "Cherelle Evatt",
+    "Lara Spainhour",
+  ];
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("IST indoor positioning",
+        style: TextStyle(color: Color(0xffffffff)),
+        ),
+
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('IST indoor positioning'),
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+              ),
+            ),
+            ListTile(
+              title: Text('Map'),
+              onTap: (){
+                // function
+                Navigator.pop(context);
+              }
+            ),
+            ListTile(
+              title: Text('Search'),
+                onTap: (){
+                  // function
+                  Navigator.pop(context);
+                }
+            ),
+            ListTile(
+              title: Text('Places of interest'),
+              onTap: (){
+                //function
+                Navigator.pushNamed(context, "/search");
+              }
+            )
+          ],
+        ),
+
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
